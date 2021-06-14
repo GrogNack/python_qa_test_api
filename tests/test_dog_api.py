9b7631330747ebf8dd3b46e5a8b6b5ff734c2f4b
mkazantsev <mkazantsev@htc-cs.ru>
2021-06-14 20:43:39 +0400

    add mock for others test

--- tests/test_dog_api.py
+++ tests/test_dog_api.py
@@ -1,47 +1,11 @@
 import json
-from unittest.mock import Mock
 from unittest.mock import patch
 
 import cerberus
 import pytest
 import requests
 
-
-def mock_request_all_bread():
-    response_mock = Mock()
-    response_mock.status_code = 200
-    response_mock.json.return_value = {
-        "message": {"affenpinscher": [], "african": [], "airedale": [], "akita": [], "appenzeller": [],
-                    "australian": ["shepherd"], "basenji": [], "beagle": [], "bluetick": [], "borzoi": [],
-                    "bouvier": [], "boxer": [], "brabancon": [], "briard": [], "buhund": ["norwegian"],
-                    "bulldog": ["boston", "english", "french"], "bullterrier": ["staffordshire"], "cairn": [],
-                    "cattledog": ["australian"], "chihuahua": [], "chow": [], "clumber": [], "cockapoo": [],
-                    "collie": ["border"], "coonhound": [], "corgi": ["cardigan"], "cotondetulear": [], "dachshund": [],
-                    "dalmatian": [], "dane": ["great"], "deerhound": ["scottish"], "dhole": [], "dingo": [],
-                    "doberman": [], "elkhound": ["norwegian"], "entlebucher": [], "eskimo": [],
-                    "finnish": ["lapphund"], "frise": ["bichon"], "germanshepherd": [], "greyhound": ["italian"],
-                    "groenendael": [], "havanese": [],
-                    "hound": ["afghan", "basset", "blood", "english", "ibizan", "plott", "walker"], "husky": [],
-                    "keeshond": [], "kelpie": [], "komondor": [], "kuvasz": [], "labradoodle": [], "labrador": [],
-                    "leonberg": [], "lhasa": [], "malamute": [], "malinois": [], "maltese": [],
-                    "mastiff": ["bull", "english", "tibetan"], "mexicanhairless": [], "mix": [],
-                    "mountain": ["bernese", "swiss"], "newfoundland": [], "otterhound": [], "ovcharka": ["caucasian"],
-                    "papillon": [], "pekinese": [], "pembroke": [], "pinscher": ["miniature"], "pitbull": [],
-                    "pointer": ["german", "germanlonghair"], "pomeranian": [],
-                    "poodle": ["miniature", "standard", "toy"], "pug": [], "puggle": [], "pyrenees": [], "redbone": [],
-                    "retriever": ["chesapeake", "curly", "flatcoated", "golden"], "ridgeback": ["rhodesian"],
-                    "rottweiler": [], "saluki": [], "samoyed": [], "schipperke": [],
-                    "schnauzer": ["giant", "miniature"], "setter": ["english", "gordon", "irish"],
-                    "sheepdog": ["english", "shetland"], "shiba": [], "shihtzu": [],
-                    "spaniel": ["blenheim", "brittany", "cocker", "irish", "japanese", "sussex", "welsh"],
-                    "springer": ["english"], "stbernard": [],
-                    "terrier": ["american", "australian", "bedlington", "border", "dandie", "fox", "irish",
-                                "kerryblue", "lakeland", "norfolk", "norwich", "patterdale", "russell", "scottish",
-                                "sealyham", "silky", "tibetan", "toy", "westhighland", "wheaten", "yorkshire"],
-                    "vizsla": [], "waterdog": ["spanish"], "weimaraner": [], "whippet": [], "wolfhound": ["irish"]},
-        "status": "success"
-    }
-    return response_mock
+from mocks.mock_dog_api import mock_request_all_bread, mock_request_images_bread, mock_request_random_image
 
 
 class TestDogAPI:
@@ -70,8 +34,9 @@ class TestDogAPI:
         assert result.status_code == 200
         assert v.validate(result.json(), schema)
 
+    @patch.object(requests, 'get', return_value=mock_request_images_bread())
     @pytest.mark.parametrize("breed", ["chow", "chihuahua", "briard", "boxer"])
-    def test_list_images_by_breed(self, breed):
+    def test_list_images_by_breed(self, mock_requests, breed):
         schema = {
             "message": {"type": "list"},
             "status": {"type": "string"}
@@ -81,24 +46,14 @@ class TestDogAPI:
         assert result.status_code == 200
         assert v.validate(result.json(), schema)
 
+    @patch('requests.get', return_value=mock_request_random_image())
     @pytest.mark.parametrize("breed", ["basenji", "borzoi", "buhund", "cattledog"])
-    def test_random_image_by_breed(self, breed):
+    def test_random_image_by_breed(self, mock_requests, breed):
         schema = {
             "message": {"type": "string"},
             "status": {"type": "string"}
         }
         result = requests.get(self.base_url + f'/breed/{breed}/images/random')
-        v = cerberus.Validator()
-        assert result.status_code == 200
-        assert v.validate(result.json(), schema)
-
-    @pytest.mark.parametrize("breed", ["basenji", "borzoi", "buhund", "cattledog"])
-    def test_all_sub_breed(self, breed):
-        schema = {
-            "message": {"type": "list"},
-            "status": {"type": "string"}
-        }
-        result = requests.get(self.base_url + f'/breed/{breed}/list')
         v = cerberus.Validator()
         assert result.status_code == 200
         assert v.validate(result.json(), schema)
